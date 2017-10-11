37107b9668e4e19d47aad12de2cdc36d772dfa1c
OlegMikhailov23 <Dkfolkin@Mikhailovs-MacBook-Air.local>
2017-10-11 20:01:15 +0300

    link to vk

--- formdark2.php
+++ formdark2.php
@@ -1,6 +1,6 @@
 <?
 if((isset($_POST['nameZv'])&&$_POST['nameZv']!="")&&(isset($_POST['emailZv'])&&$_POST['emailZv']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = 'dkfolkin@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $to = 'dkfolkin@gmail.com,info@cleanprofit.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Обратный звонок'; //Загаловок сообщения
         $message = '
                 <html>
