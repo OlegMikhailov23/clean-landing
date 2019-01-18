5758f0b8f346426186aa7d2120ccbd581cb77978
Dkfolkin <dkfolkin@gmail.com>
2019-01-18 18:48:00 +0300

    добавлен прелоад

---
+++ build/source/js/main1.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($){
+	//create the slider
+	$('.cd-testimonials-wrapper').flexslider({
+		selector: ".cd-testimonials > li",
+		animation: "slide",
+		controlNav: false,
+		slideshow: false,
+		smoothHeight: true,
+		start: function(){
+			$('.cd-testimonials').children('li').css({
+				'opacity': 1,
+				'position': 'relative'
+			});
+		}
+	});
+
+
+
+	//close the testimonials modal page
+	$('.cd-testimonials-all .close-btn').on('click', function(){
+		$('.cd-testimonials-all').removeClass('is-visible');
+	});
+	$(document).keyup(function(event){
+		//check if user has pressed 'Esc'
+    	if(event.which=='27'){
+    		$('.cd-testimonials-all').removeClass('is-visible');	
+	    }
+    });
+    
+	//build the grid for the testimonials modal page
+	$('.cd-testimonials-all-wrapper').children('ul').masonry({
+  		itemSelector: '.cd-testimonials-item'
+	});
+});
